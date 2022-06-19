cfbb1e2704e457a2d8ceeba69ea713e600084e16
Spandan-Bhattarai <sbhattarai50@gmail.com>
2022-06-19 15:16:48 +0545

    little hint

---
+++ git(day 9).js
@@ -0,0 +1,21 @@
+git init //Initialize git. creates .git flder
+
+git remote add origin <github_repo_url> //links our remote repoository and local repository
+
+git remote -v //to check our linked remote repo
+
+git status // to check our changes
+
+git add <file path> //to add our single file for comit
+git add . //to add all of untracked files
+
+git commit -m "short message" //git commit -m "short message -m "detailed message"
+
+git branch //llocal branch
+git branch /r // remote branch
+
+git checkout -b <new_branch_name> 
+
+git push origin master//to add to master branch
+//
+https://github.com/Spandan-Bhattarai
